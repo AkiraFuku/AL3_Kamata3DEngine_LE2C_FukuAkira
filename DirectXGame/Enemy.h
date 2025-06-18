2da97dd71e07739185deaac4c894c2a3da1c2f5f
AkiraFuku <k024g1002@g.neec.ac.jp>
2025-06-18 12:31:27 +0900

    a

---
+++ DirectXGame/Enemy.h
@@ -0,0 +1,17 @@
+#pragma once
+#include "KamataEngine.h"
+#include "MassFunction.h"
+#include <numbers>
+#include <algorithm>
+class Enemy {
+
+	public:
+
+	void Initialize();
+	
+	void Update();
+	
+	void Draw();
+
+
+};
