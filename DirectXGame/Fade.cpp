4e57fcd9325eedf8e7966a9d2411586753ddecd1
AkiraFuku <k024g1002@g.neec.ac.jp>
2025-07-04 10:53:30 +0900

    20p

--- DirectXGame/Fade.cpp
+++ DirectXGame/Fade.cpp
@@ -1,16 +1,37 @@
-#include "Fade.h"
+#include "Fade.h"  
+#include <algorithm>  
+void Fade::Initialize(){  
+	sprite_= Sprite::Create(0,Vector2{});  
+	sprite_->SetSize(Vector2(WinApp::kWindowWidth, WinApp::kWindowHeight));  
+	sprite_->SetColor(Vector4(0, 0, 0,1.0f));  
+};  
+void Fade::Update(){  
+	switch (status_) {  
+	case Fade::Status::None:  
+		break;  
+	case Fade::Status::FadeIn:  
 
-void Fade::Initialize(){
-	sprite_= Sprite::Create(0,Vector2{});
-	sprite_->SetSize(Vector2(WinApp::kWindowWidth, WinApp::kWindowHeight));
-	sprite_->SetColor(Vector4(0, 0, 0,1.0f));
-};
-void Fade::Update(){
-
-};
-void Fade::Draw() {
-	Sprite::PreDraw(DirectXCommon::GetInstance()->GetCommandList());
-	sprite_->Draw();
-	Sprite::PostDraw();
-
+		break;  
+	case Fade::Status::FadeOut:  
+		// FadeOutの処理  
+		// カウンターを進める  
+		counter_ += 1.0f / 60.0f;  
+		// カウンターがdurationを超えた打ち止め  
+		if (counter_>= duration_) {  
+			counter_ = duration_;  
+		}  
+		// アルファ値を計算  
+		sprite_->SetColor(Vector4(0, 0, 0, std::clamp(counter_ / duration_, 0.0f, 1.0f)));  
+		break;  
+	}  
+};  
+void Fade::Draw() {  
+	Sprite::PreDraw(DirectXCommon::GetInstance()->GetCommandList());  
+	sprite_->Draw();  
+	Sprite::PostDraw();  
+}  
+void Fade::Start(Status status, float duration) {  
+	status_= status;  
+	duration_ = duration;  
+	counter_ = 0.0f;  
 };
--- DirectXGame/Fade.h
+++ DirectXGame/Fade.h
@@ -3,10 +3,22 @@
 using namespace KamataEngine;
 class Fade {
 	public:
+	enum class Status {
+		None,
+		FadeIn,
+		FadeOut
+
+	};
+
 		void Initialize();
 		void Update();
 		void Draw();
-
+		void Start(Status status, float duration) ;
 	private:
 	    Sprite* sprite_ = nullptr;
+	    Status status_ = Status::None;
+	    // フェードの持続時間
+	    float duration_ = 0.0f;
+	    // フェードの経過時間
+	    float counter_ = 0.0f;
 };
