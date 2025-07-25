c186335f5d31acadb1104823b94b9f53a3ff888f
AkiraFuku <k024g1002@g.neec.ac.jp>
2025-07-25 12:06:45 +0900

    29p

--- DirectXGame/HitEffect.cpp
+++ DirectXGame/HitEffect.cpp
@@ -16,7 +16,7 @@ void HitEffect::Initialize(const Vector3& position){
 	std::uniform_real_distribution<float> rotationDistribution(-std::numbers::pi_v<float>, std::numbers::pi_v<float>);
 
 	for (WorldTransform& worldTransform:ellipseWorldTransforms_) {
-		worldTransform.scale_={0.1f,1.5f,1.0f};
+		//worldTransform.scale_={0.1f,1.5f,1.0f};
 		worldTransform.rotation_={0.0f,0.0f,rotationDistribution(randomEngine)};
 		worldTransform.translation_=position;
 		worldTransform.Initialize();
@@ -24,8 +24,43 @@ void HitEffect::Initialize(const Vector3& position){
 	circleWorldTransform_.translation_=position;
 	circleWorldTransform_.Initialize();
 	circleWorldTransform_.translation_.z = -1.0f;
+	objectColor_.Initialize();
 }
 void HitEffect::Update(){
+	if (isDead()) {
+	 return;
+	}
+	switch (state_) {
+	case HitEffect::State::kSpread:{
+		++counter_;
+		float scale= 0.5f+ static_cast<float>(counter_)/kSpreadTime*0.5;
+		const float slashScale = 2.0f;
+		for (WorldTransform& worldTransform:ellipseWorldTransforms_) {
+			worldTransform.scale_={0.1f,scale*slashScale,1.0f};
+		}
+			const float circleScale = 1.0;
+
+		circleWorldTransform_.scale_ = {scale * circleScale, scale * circleScale, 1.0f};
+		if ( counter_ >= kSpreadTime) {
+			state_ = State::kFade;
+			counter_ = 0; // カウンターをリセット
+		}
+		break;
+	}
+	case HitEffect::State::kFade:{
+			++counter_;
+		objectColor_.SetColor(Vector4{1.0f, 1.0f, 1.0f, 1.0f - static_cast<float>(counter_) / kFadeTime});
+
+		if (++counter_ >= kFadeTime) {
+			state_ = State::kDead;
+		}
+
+		break;
+	}
+		default:
+		break;
+	}
+
 for (WorldTransform& worldTransform:ellipseWorldTransforms_) {
 	WorldTransformUpdate(&worldTransform);
 }
@@ -34,10 +69,13 @@ WorldTransformUpdate(&circleWorldTransform_);
 void HitEffect::Draw(){
 	assert(model_);
 	assert(camera_);
-	for (WorldTransform& worldTransform:ellipseWorldTransforms_) {
-		model_->Draw(worldTransform,*camera_);
+	if (isDead()) {
+	 return;
 	}
-	model_->Draw(circleWorldTransform_,*camera_);
+	for (WorldTransform& worldTransform:ellipseWorldTransforms_) {
+		model_->Draw(worldTransform,*camera_,&objectColor_);
+	}
+	model_->Draw(circleWorldTransform_,*camera_,&objectColor_);
 }
 HitEffect* HitEffect::Create(const Vector3& position){ 
 	HitEffect* instance = new HitEffect();
--- DirectXGame/HitEffect.h
+++ DirectXGame/HitEffect.h
@@ -3,18 +3,42 @@
 using namespace KamataEngine;
 class HitEffect {
 public:
-	
+	enum class State{
+		kSpread,
+		kFade,
+		kDead
+	};
+
 	void Update();
 	void Draw() ;
 	static void SetModel(Model* model) { model_ = model; }
 	static void SetCamera(Camera* camera) { camera_ = camera; }
 	static HitEffect* Create(const Vector3& position) ;
+
+	bool isDead() const {return state_==State::kDead;}
 private:
-void Initialize(const Vector3& position) ;
+	HitEffect()=default;
+
+	void Initialize(const Vector3& position) ;
+
+	static inline const uint32_t kSpreadTime = 10;
+
+	static inline const uint32_t kFadeTime = 20;
+
+	static inline const uint32_t kLifetime = kSpreadTime + kFadeTime;
+	
 	static Model* model_ ;
 	static Camera* camera_;
 	// 円形エフェクト
 	WorldTransform circleWorldTransform_;
+	//楕円形エフェクト
 	static const inline uint32_t kellipseEffectNum = 2;
 	std::array<WorldTransform,kellipseEffectNum>ellipseWorldTransforms_;
+
+	State state_=State::kSpread;
+
+	uint32_t counter_=0;
+
+	ObjectColor objectColor_;
+
 };
